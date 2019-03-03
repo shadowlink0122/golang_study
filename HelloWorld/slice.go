05ec83adbd2deae9c6a19c221dc45f74dbc93989
Miyajima Kenta <shadowlink0122@gmail.com>
2019-03-03 17:35:17 +0900

    Test code for slice

---
+++ HelloWorld/slice.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := [...]int{1, 2, 3, 4, 5, 6}
+
+	b := a[:]
+	c := a[2:]
+	d := a[:4]
+	e := a[2:4]
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+}
